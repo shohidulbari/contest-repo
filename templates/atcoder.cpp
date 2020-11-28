a0bf92341da50c38887c77a4348c2ad4dc89d360
shohidulbari <shohidulbari18@gmail.com>
2020-11-28 07:32:15 +0600

    first commit

---
+++ templates/atcoder.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+typedef long long ll;
+
+#define forn(i, a, b) for(int i=a; i<b; i++)
+#define pb push_back
+#define F first
+#define S second
+
+void solve(){
+    
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(nullptr);
+    solve();
+    return 0;
+}
---
+++ templates/codeforces.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+typedef long long ll;
+
+#define forn(i, a, b) for(int i=a; i<b; i++)
+#define pb push_back
+#define F first
+#define S second
+
+void solve(){
+    
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(nullptr);
+    int test;
+    cin >> test;
+    for(int it=1;it<=test;it++) {
+        solve();
+    }
+    return 0;
+}
