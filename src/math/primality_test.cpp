5e864bfbb39a7e4908a48292b40c00588d3deb7b
shohidulbari <shohidulbari18@gmail.com>
2021-01-03 22:14:49 +0600

    update primality-test miller rabin

--- src/math/primality_test.cpp
+++ src/math/primality_test.cpp
@@ -5,6 +5,7 @@ using namespace std;
 typedef long long ll;
 
 
+
 //Trial Division...Improved... All Prime Number Above 3 are in either 6k + 1 or 6k + 5 form
 bool isPrime(ll n) {
     if (n < 2ll) return false;
@@ -57,32 +58,48 @@ bool isPrimeFermet(ll n, ll iter = 5){
 *a^(2^r.d) = -1 mod n holds for 0<=r<=s-1
 */
 
-bool isComposite(ll n, ll a, ll d, ll s){
-    ll x = binpower(a, d, n);
-    if(x == 1ll or x == n - 1)
+using u64 = long long;
+using u128 = __uint128_t;
+
+u64 calcpower(u64 a, u64 b, u64 n) {
+    a = a % n;
+    u64 res = 1;
+    while (b) {
+        if (b & 1)
+            res = (u128)res * a % n;
+        a = (u128)a * a % n;
+        b >>= 1;
+    }
+    return res;
+}
+
+bool isComposite(u64 n, u64 a, u64 d, int s) {
+    u64 x = calcpower(a, d, n);
+    if (x == 1 || x == n - 1)
         return false;
-    for(int r = 1; r<s; r++){
-        x = x * x % n;
-        if(x == n - 1)
+    for (int r = 1; r < s; r++) {
+        x = (u128)x * x % n;
+        if (x == n - 1)
             return false;
     }
     return true;
-}
+};
 
 //Return's True if n is probably prime
-bool isPrimeMillerRabin(ll n, ll iter = 5){
-    if(n < 4ll)
-        return n == 2ll or n == 3ll;
-    ll s = 0;
-    ll d = n - 1ll;
-    while(d & 1ll == 0){
-        d = d >> 1;
+bool isPrimeMillerRabin(u64 n, int iter=5) { // returns true if n is probably prime, else returns false.
+    if (n < 4)
+        return n == 2 || n == 3;
+
+    int s = 0;
+    u64 d = n - 1;
+    while ((d & 1) == 0) {
+        d >>= 1;
         s++;
     }
 
-    for(int i=0; i<iter; i++){
-        ll a = 2 + rand() % (n - 3);
-        if(isComposite(n, a, d, s))
+    for (int i = 0; i < iter; i++) {
+        int a = 2 + rand() % (n - 3);
+        if (isComposite(n, a, d, s))
             return false;
     }
     return true;
