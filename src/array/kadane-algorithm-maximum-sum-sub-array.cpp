bb8b636046df48fe804cdb823df65aaeb8531dbb
shohidulbari <shohidulbari18@gmail.com>
2021-01-05 18:48:27 +0600

    fix kadane algorithm

--- src/array/kadane-algorithm-maximum-sum-sub-array.cpp
+++ src/array/kadane-algorithm-maximum-sum-sub-array.cpp
@@ -9,6 +9,13 @@ int kadane(vector<int>& vals){
     for(int i=0; i< vals.size(); i++){
         local_max = local_max + vals[i];
         global_max = max(global_max, local_max);
+        if(local_max < 0) local_max = 0;
     }
     return global_max;
+}
+
+int main(){
+    vector<int> v{-1, 2, 3, -3, 3, 4, 5, -3};
+    int res = kadane(v);
+    assert(res == 14);
 }
